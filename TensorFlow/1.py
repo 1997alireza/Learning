e17e511230759c24bbcabb8c28ea5daa1e072fb2
1997alireza <torabian.alireza@gmail.com>
2018-09-12 01:28:09 +0430

    run with placeholder on tensorflow

--- TensorFlow/1.py
+++ TensorFlow/1.py
@@ -1,10 +1,14 @@
 import os
 import tensorflow as tf
+import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 hello = tf.constant('hello')
+
+# A session allows to execute graphs or part of graphs. It allocates resources.
 sess = tf.Session()
 print(sess.run(hello))
+print(tf.constant(23) + tf.constant(9))
 print(sess.run(tf.constant(23) + tf.constant(9)))
 sess.close()
---
+++ TensorFlow/2.py
@@ -0,0 +1,15 @@
+import os
+import tensorflow as tf
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+sess = tf.Session()
+b = tf.Variable(tf.zeros(100,))
+w = tf.Variable(tf.random_uniform((748, 100), -1, 1))
+x = tf.placeholder(tf.float32, (100, 748))
+h = tf.nn.relu(tf.matmul(x, w) + b)
+sess.run(tf.global_variables_initializer())
+print(sess.run(h, {x: np.random.random_sample((100, 748))}))
+
+sess.close()
