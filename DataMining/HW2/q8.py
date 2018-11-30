853bd70c05931f70b4099c94b654c90978bdb189
1997alireza <torabian.alireza@gmail.com>
2018-11-30 13:04:14 +0330

    plot errors base on number of nodes in the decision tree

--- DataMining/HW2/q8.py
+++ DataMining/HW2/q8.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from tools import entropy, norm2_error
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 
 class Node:
@@ -106,39 +108,76 @@ class Node:
             return self.children[child_index].get_class(x)
         return self.most_populous  # for other options that hadn't seen in the train data
 
+    def get_number_of_nodes(self):
+        if self.feature_splitter_index == -1:
+            return 1
+        return sum([child.get_number_of_nodes() for child in self.children]) + 1
+
 
 # MAX_DEPTH = 700
 
 
 if __name__ == '__main__':
+    dataset = pd.read_csv('noisy_valid.csv')
+    v_x = dataset.iloc[:, 1:].values.tolist()
+    v_y = dataset.iloc[:, 0].values.tolist()
     dataset = pd.read_csv('noisy_train.csv')
-    x = dataset.iloc[:, 1:].values.tolist()
-    y = dataset.iloc[:, 0].values.tolist()
+    tr_x = dataset.iloc[:, 1:].values.tolist()
+    tr_y = dataset.iloc[:, 0].values.tolist()
+    dataset = pd.read_csv('noisy_test.csv')
+    te_x = dataset.iloc[:, 1:].values.tolist()
+    te_y = dataset.iloc[:, 0].values.tolist()
 
-    num_of_features = len(x[0])
+    num_of_features = len(v_x[0])
 
     class0_data = []
     class1_data = []
-    for i in range(len(y)):
-        if y[i] == 0:
-            class0_data.append(x[i])
+    for i in range(len(v_y)):
+        if v_y[i] == 0:
+            class0_data.append(v_x[i])
         else:
-            class1_data.append(x[i])
+            class1_data.append(v_x[i])
 
-    decision_tree_r = Node(class0_data, class1_data, satisfying_gain=.005)
-    decision_tree_r.make_best_splitter()
+    number_of_nodes = []
+    valid_errs = []
+    train_errs = []
+    test_errs = []
+    for i in range(1000):
+        satisfying_gain = i / 1000
+        print('satisfying gain:', satisfying_gain)
+        decision_tree_r = Node(class0_data, class1_data, satisfying_gain=satisfying_gain)
+        decision_tree_r.make_best_splitter()
 
-    y_pred_on_train = []
-    for i in range(len(x)):
-        y_pred_on_train.append(decision_tree_r.get_class(x[i]))
-    print('Error on train data (euclidean distance): ', norm2_error(y, y_pred_on_train))
+        # print('Number of nodes:', decision_tree_r.get_number_of_nodes())
+        number_of_nodes.append(decision_tree_r.get_number_of_nodes())
 
-    dataset = pd.read_csv('noisy_test.csv')
-    x = dataset.iloc[:, 1:].values.tolist()
-    y = dataset.iloc[:, 0].values.tolist()
-    y_pred_on_test = []
-    for i in range(len(x)):
-        y_pred_on_test.append(decision_tree_r.get_class(x[i]))
-    print('Error on test  data (euclidean distance): ', norm2_error(y, y_pred_on_test))
+        y_pred_on_valid = []
+        for i in range(len(v_x)):
+            y_pred_on_valid.append(decision_tree_r.get_class(v_x[i]))
+        # print('Error on valid data (euclidean distance): ', norm2_error(v_y, y_pred_on_valid))
+        valid_errs.append(norm2_error(v_y, y_pred_on_valid))
 
+        y_pred_on_train = []
+        for i in range(len(tr_x)):
+            y_pred_on_train.append(decision_tree_r.get_class(tr_x[i]))
+        # print('Error on train data (euclidean distance): ', norm2_error(tr_y, y_pred_on_train))
+        train_errs.append(norm2_error(tr_y, y_pred_on_train))
 
+        y_pred_on_test = []
+        for i in range(len(te_x)):
+            y_pred_on_test.append(decision_tree_r.get_class(te_x[i]))
+        # print('Error on test  data (euclidean distance): ', norm2_error(te_y, y_pred_on_test))
+        test_errs.append(norm2_error(te_y, y_pred_on_test))
+
+    plt.plot(number_of_nodes, valid_errs, 'b')
+    plt.plot(number_of_nodes, train_errs, 'g')
+    plt.plot(number_of_nodes, test_errs, 'r')
+
+    plt.legend(handles=[
+        mpatches.Patch(color='blue', label='valid'),
+        mpatches.Patch(color='green', label='train'),
+        mpatches.Patch(color='red', label='test')
+    ])
+    plt.xlabel('Number of nodes')
+    plt.ylabel('Error (euclidean distance)')
+    plt.show()
